7e258bd68066011c2e8a07691cc0b9c3f7aafc89
felipetaua <taua.felipee@gmail.com>
2025-05-13 11:41:10 -0300

    adicionando animações e adicionando um scroll para descer na navegação

--- assets/js/global/main.js
+++ assets/js/global/main.js
@@ -4,3 +4,30 @@ const nav = document.querySelector('.nav-bar');
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('ativo');
 });
+
+const config = {
+    duration: 1200,      // tempo da animação
+    distance: '50px',    // distância do movimento
+    easing: 'ease',
+    origin: 'top',    // direção de onde o elemento vem
+    opacity: 0,
+    interval: 80,
+    reset: false 
+};
+
+
+// animações do painel de funcionalidades
+ScrollReveal().reveal('.cardOne', config);
+ScrollReveal().reveal('.cardTwo', config);
+ScrollReveal().reveal('.cardThree', config);
+ScrollReveal().reveal('.cardFour', config);
+ScrollReveal().reveal('.cardFive', config);
+
+
+
+// Animações do celulares
+ScrollReveal().reveal('.cellA1', config);
+ScrollReveal().reveal('.cellA2', config);
+ScrollReveal().reveal('.cellA3', config);
+ScrollReveal().reveal('.cellA4', config);
+ScrollReveal().reveal('.cellA5', config);
