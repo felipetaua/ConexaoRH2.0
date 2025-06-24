1f1258ad86b3b804700ac710b4fa4735fbb72a73
felipetaua <taua.felipee@gmail.com>
2025-06-24 13:13:47 -0300

    refatornando o login e adicionando o js

--- assets/js/pages/login/login.js
+++ assets/js/pages/login/login.js
@@ -1,17 +1,10 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    if (email) {
+        alert(`Login bem-sucedido para o e-mail: ${email} (Isso é uma demonstração)`);
+    } else {
+        alert('Por favor, preencha o campo de e-mail.');
+    }
 });
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
-
-
-// function main() {
-//     window.location = "../main/main.html";
-// }
